6f642b0297365f6c214bfdcb15ea990e6801dead
Marie Enrile <mariekaren.enrile@gmail.com>
2024-03-22 02:14:55 -0400

    Update Home.jsx
    minimal changes  in home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import house1 from "../assets/house1.jpg";
+import house2 from "../assets/house2.jpg";
+import house3 from "../assets/house3.jpg";
+import house4 from "../assets/house4.jpg";
 
 export default function Home() {
   return (
-    <div className="my-6">
-      Home
-      <div className="my-6">
-        <Link to={"/book"}>
-          <button className="bg-slate-300 p-3 rounded-xl">Book Now</button>
-        </Link>
+    <div className="my-6 px-5">
+      {/* insert listings here */}
+      <div>
+        <div className="bg-gray-100">
+          <img src={house1} />
+        </div>
+        Arianna's Pet-Friendly Home
       </div>
     </div>
   );
