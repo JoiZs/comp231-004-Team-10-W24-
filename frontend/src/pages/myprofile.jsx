d42c306a2e54ecea1043796b3b84c05623ff0d61
JoiZs <pyaesoneaung1126@hotmail.com>
2024-04-14 15:34:14 -0400

    fix(myprofile): request url

--- frontend/src/pages/myprofile.jsx
+++ frontend/src/pages/myprofile.jsx
@@ -9,9 +9,12 @@ export default function Profile() {
 
   const fetchProfileData = async () => {
     try {
-      const response = await axios.get("http://localhost:4000/profile/me", {
-        withCredentials: true,
-      });
+      const response = await axios.get(
+        import.meta.env.VITE_SERV_URL + "/profile/me",
+        {
+          withCredentials: true,
+        }
+      );
       setProfileData(response.data.data);
       setIsLoading(false);
       console.log("Pet types:", response.data.data.Profile.petType); // Logs the pet types
